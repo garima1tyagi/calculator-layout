2ee16d55ab7c10cd22afdd90bd092b303a5ee0bc
Garima Tyagi <garima408@students.itu.edu>
2020-01-18 17:55:45 -0800

    added zoom effect on images on hover

--- src/cards/CardUI.js
+++ src/cards/CardUI.js
@@ -3,7 +3,9 @@ import React, {Component} from 'react'
 const CardUI = (props) => {
     return(
         <div className=" card card-content-center">
+        <div className="overflow">
         <img src= {props.imgSrc} alt={props.altName} className="card-img-top"></img>
+        </div>
             <div className="overflow card-body">
             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                 <button className="btn btn-success">More Info</button>
